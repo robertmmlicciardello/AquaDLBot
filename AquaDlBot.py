d23192b3e43b79a8184baead579c81161a4b7729
robertmmlicciardello <86592959+robertmmlicciardello@users.noreply.github.com>
2022-01-25 03:11:35 +0630

    Update AquaDlBot.py

--- AquaDlBot.py
+++ AquaDlBot.py
@@ -8,9 +8,9 @@ from os import getenv
 #Developed by @AsiaXDev
 #repo -> https://github.com/AsiaXDev/AquaDlBot
 
-BOT_TOKEN = os.getenv("5202241387:AAFsaXnu_gZqyyFv9M4_f05jj0QdU44gV2U")
-API_ID = os.getenv("4557817")
-API_HASH = os.getenv("d74e82c8f8ab7c6a65306bc82ff916ed")
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+API_ID = os.getenv("API_ID")
+API_HASH = os.getenv("API_HASH")
 
 Aqua = Client(
     "gtotfuckbot",
